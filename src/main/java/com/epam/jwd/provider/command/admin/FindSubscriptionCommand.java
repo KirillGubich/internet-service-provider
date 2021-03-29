b5abc8242c1673f93d5a94d90bc7b9e688e39d07
Kirill <60620490+KirillGubich@users.noreply.github.com>
2021-03-29 23:46:47 +0300

    View subscriptions by user id

--- src/main/java/com/epam/jwd/provider/command/admin/FindSubscriptionCommand.java
+++ src/main/java/com/epam/jwd/provider/command/admin/FindSubscriptionCommand.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Searches for a subscription by a given ID.
+ * Searches for subscriptions by a given ID or user ID.
  * The search result is displayed on the page or a corresponding no result message.
  */
 public enum FindSubscriptionCommand implements Command {
@@ -31,23 +31,43 @@ public enum FindSubscriptionCommand implements Command {
     };
 
     private static final String ID_PARAMETER_NAME = "id";
+    private static final String USER_ID_PARAMETER_NAME = "userId";
     private static final String USER_SUBSCRIPTIONS_ATTRIBUTE_NAME = "userSubscriptions";
+    private final SubscriptionService service = RealSubscriptionService.INSTANCE;
 
     @Override
     public ResponseContext execute(RequestContext request) {
+        String idParameter = request.getParameter(ID_PARAMETER_NAME);
+        List<SubscriptionDto> subscriptions = new ArrayList<>();
+        Optional<SubscriptionDto> subscription = findSubscriptionByIdParameter(idParameter);
+        if (!subscription.isPresent()) {
+            String userIdParameter = request.getParameter(USER_ID_PARAMETER_NAME);
+            List<SubscriptionDto> userSubscriptions = findSubscriptionByUserIdParameter(userIdParameter);
+            subscriptions.addAll(userSubscriptions);
+        } else {
+            subscriptions.add(subscription.get());
+        }
+        request.setAttribute(USER_SUBSCRIPTIONS_ATTRIBUTE_NAME, subscriptions);
+        return USERS_SUBSCRIPTION_SETTINGS_PAGE_RESPONSE;
+    }
+
+    private Optional<SubscriptionDto> findSubscriptionByIdParameter(String idParameter) {
         int id;
         try {
-            id = Integer.parseInt(request.getParameter(ID_PARAMETER_NAME));
+            id = Integer.parseInt(idParameter);
         } catch (NumberFormatException e) {
-            return USERS_SUBSCRIPTION_SETTINGS_PAGE_RESPONSE;
+            return Optional.empty();
         }
-        SubscriptionService service = RealSubscriptionService.INSTANCE;
-        Optional<SubscriptionDto> subscriptionDto = service.findById(id);
-        List<SubscriptionDto> subscriptions = new ArrayList<>();
-        if (subscriptionDto.isPresent()) {
-            subscriptions.add(subscriptionDto.get());
-            request.setAttribute(USER_SUBSCRIPTIONS_ATTRIBUTE_NAME, subscriptions);
+        return service.findById(id);
+    }
+
+    private List<SubscriptionDto> findSubscriptionByUserIdParameter(String userIdParameter) {
+        int id;
+        try {
+            id = Integer.parseInt(userIdParameter);
+        } catch (NumberFormatException e) {
+            return new ArrayList<>();
         }
-        return USERS_SUBSCRIPTION_SETTINGS_PAGE_RESPONSE;
+        return service.findUserSubscriptions(id);
     }
 }
